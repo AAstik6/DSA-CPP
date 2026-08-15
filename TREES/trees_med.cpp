de1db8a429ed7968afa6d4f7382065e10b09b5ee
AAstik6 <aastik829@gmail.com>
2026-08-15 17:28:45 +0530

    Tree level order questions

--- TREES/trees_med.cpp
+++ TREES/trees_med.cpp
@@ -40,4 +40,63 @@ public:
         }
         return res;
     }
+
+    // 103. Binary Tree Zigzag Level Order Traversal
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        queue<TreeNode*> q;
+        int check = 1;
+        vector<vector<int>> res;
+        if (root == NULL) return res;
+        q.push(root);
+        
+        while (!q.empty()) {
+            int qsize = q.size();
+            vector<int> temp(qsize, 0);
+            int front = 0;
+            int last = qsize - 1;
+            while (qsize > 0) {
+                TreeNode* t = q.front();
+                q.pop();
+                if (check == 1) {
+                    temp[front] = t->val;
+                    front++;
+                }
+                else {
+                    temp[last] = t->val;
+                    last--;
+                }
+                if (t->left != NULL) q.push(t->left);
+                if (t->right != NULL) q.push(t->right);
+                qsize--;
+            }
+            if (check == 1) check = 0;
+            else if (check == 0) check = 1;
+            res.push_back(temp);
+        }
+        return res;
+    }
+
+    // 107. Binary Tree Level Order Traversal II
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        queue<TreeNode*> qu;
+        vector<vector<int>> res;
+        if (root == NULL) return res;
+        qu.push(root);
+        while (!qu.empty()) {
+            int qsize = qu.size();
+            vector<int> temp;
+            while (qsize > 0) {
+                TreeNode* t = qu.front();
+                qu.pop();
+                temp.push_back(t->val);
+                if (t->left != NULL) qu.push(t->left);
+                if (t->right != NULL) qu.push(t->right);
+                qsize--;
+            }
+            res.push_back(temp);
+        }
+        reverse(res.begin(), res.end());
+        return res;
+        
+    }
 };
